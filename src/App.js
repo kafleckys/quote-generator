de704f39afb3ba89f2e50844cd7a12ca4291857b
Prakash <kafleprakash0@gmail.com>
2023-06-03 11:46:58 +0545

    Add share buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import { TwitterShareButton, WhatsappShareButton } from "react-share";
 
 const App = () => {
+  const shareUrl = "https://twitter.com";//place actual url here to share quotes
+  const shareUrl1 = "https://whatsapp.com";//this is the place of actual url
+
+
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +37,27 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="sharebtns">
+          <div className="share">
+            <TwitterShareButton url={shareUrl} title={quote}>
+              Share on Twitter
+            </TwitterShareButton>
+          </div>
+
+          <div className="share">
+            <WhatsappShareButton url={shareUrl1} title={quote}>
+              Share on WhatsApp
+            </WhatsappShareButton>
+          </div>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
